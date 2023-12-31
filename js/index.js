35a700e199d9720a608f2b482e3a733c61e56dbd
matheusguerreiro <prosadeteclas@gmail.com>
2023-12-31 18:14:21 -0300

    render cards e etc ...

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+export async function fetchData() {
+  try {
+    const res = await fetch('../data.json');
+
+    if (!res.ok) {
+      throw new Error(`Erro ao carregar o arquivo JSON: ${res.status}`);
+    }
+
+    return await res.json();
+
+  } catch (err) {
+    console.error(err.message);
+  }
+}
